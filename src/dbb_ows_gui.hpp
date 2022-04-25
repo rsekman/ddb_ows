ce37b4b03ec8dd37df7546a4b0f74d42bd61c32b
Robin Ekman <robin.seth.ekman@gmail.com>
2022-04-25 20:32:11 +0200

    refactoring + preparing Makefile for building gui plugins

---
+++ src/dbb_ows_gui.hpp
@@ -0,0 +1,9 @@
+#include <gtk/gtk.h/>
+
+#include <deadbeef/deadbeef.h>
+#include <deadbeef/plugins/converter/converter.h>
+#include <deadbeef/plugins/gtkui/gtkui_api.h>
+
+namespace ddb_ows_gui {
+
+}
