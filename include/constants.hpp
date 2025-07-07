438ec6727184453ed92a2e8b7ae037219627041e
Robin Ekman <robin.seth.ekman@gmail.com>
2025-07-07 06:09:59 +0200

    bump version number in preparation for sqlite refactoring

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,7 +2,7 @@
 #define DDB_OWS_CONSTANTS_HPP
 
 #define DDB_OWS_VERSION_MAJOR 0
-#define DDB_OWS_VERSION_MINOR 2
+#define DDB_OWS_VERSION_MINOR 4
 #define DDB_OWS_VERSION_PATCH 0
 
 // dark preprocessor magic
